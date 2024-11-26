cfcc4e19cf1d31ac12e913f564f7c23ca9789ad5
Thomas Wang <qishwang123@gmail.com>
2024-11-26 15:55:31 -0500

    init

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Thomas was Here</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          hello, my name is thomasasdlk fjaslfk jasklfj asdlkf jasl;kfj asdlkfj asdklf jadlskf jaldsjf klasj flk;asdj flkasj flkajdsflk; jadsklf jasdlkf jalsdkf jadsl;kf jlaksd; fjkalds fjal;kdsfj aslkd;fj lka;ds jflakdsj flak;dsjf lkadsfj asdlkfj aldks;f jlkasd jflkasd jflk;asj flk;asj lk;asd jflk;adsf jl
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
